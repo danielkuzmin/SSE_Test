5caf895673a98d6656cd8e85dec7654892a1e2d4
Daniel Kuzmin <danielkuzmin@gmail.com>
2019-09-30 09:33:37 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+// Quick program to test Intel's SSE stuff
+#include <stdio.h>
+#include <emmintrin.h>
+#include <stdint.h>
+
+void double_array(uint32_t *p, int elems) {
+    while (elems >=4) {
+        __m128i tmp = _mm_loadu_si128((__m128i *)p);
+         tmp = _mm_add_epi32(tmp, tmp);
+         _mm_storeu_si128((__m128i *)p, tmp);
+         p = p + 4;
+         elems = elems - 4;
+    }
+
+    for (int i = 0; i < elems; i++) {
+        p[i] = p[i] + p[i];
+    }
+}
+
+int main() {
+    printf("Hello, World!\n");
+    uint32_t buf[] = {1,2,3,4,5,6};
+    double_array(buf, 6);
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d", buf[i]);
+    }
+    return 0;
+}
